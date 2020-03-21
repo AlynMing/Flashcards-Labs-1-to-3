ebf1273ec220f9f0f28f04be57ef2a95f22ff24f
Moises Limon <moises.m.limon@gmail.com>
2020-03-21 12:49:58 -0700

    I have completed Lab 3 of Codeworks iOS Mobile App Design Challenge, check the ReadMe.md for further explanation

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Flashcard {
+    var question: String
+    var answer: String
+}
 
+class ViewController: UIViewController {
+    
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    //Array to hold our flashcards
+    var flashcards = [Flashcard]()
+    
+    //Current flashcard index
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Read saved flashcards
+        readSavedFlashcards()
+        
+        //Adding our initial flashcard if needed
+        if flashcards.count == 0 {
+            updateFlashcard(question: "What's the capital of Brazil?", answer: "Brasilia")
+        } else{
+            updateLabels()
+            updateNextPrevButtons()
+        }
     }
-
+    
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if frontLabel.isHidden == false {
             frontLabel.isHidden = true
@@ -28,18 +53,148 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func didTapOnNext(_ sender: Any) {
+        
+        //increment current index
+        currentIndex = currentIndex + 1
+        
+        //update labels
+        updateLabels()
+        
+        //Updates buttons
+        updateNextPrevButtons()
+    }
+    
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        //decrement current index
+        currentIndex = currentIndex - 1
+        
+        //update labels
+        updateLabels()
+        
+        //Updates buttons
+        updateNextPrevButtons()
+    }
+    
+    @IBAction func didTapOnDelete(_ sender: Any) {
+        
+        //show confirmation
+        let alert = UIAlertController(title: "Delete flashcard", message: "Are you sure you want to delete it?", preferredStyle: .actionSheet)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) {
+            action in self.deleteCurrentFlashcard()
+        }
+        
+        alert.addAction(deleteAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func deleteCurrentFlashcard(){
+        //Delete current
+        flashcards.remove(at: currentIndex)
+        
+        //Special case: Check if last card was deleted
+        
+        if currentIndex > flashcards.count - 1 {
+            currentIndex = flashcards.count - 1
+        }
+        
+        updateNextPrevButtons()
+        updateLabels()
+        saveAllFlashcardsToDisk()
+    }
+    
     func updateFlashcard(question: String, answer: String) {
-        frontLabel.text = question
-        backLabel.text = answer
+        
+        let flashcard = Flashcard(question: question, answer: answer)
+        //Adding flashcard into the flashcards array
+        
+        flashcards.append(flashcard)
+        
+        //logging to console
+        print("Added new flashcard (: ")
+        print("We now have \(flashcards.count) flashcard(s)")
+        currentIndex = flashcards.count - 1
+        print("The current index is \(currentIndex)")
+        
+        // updates buttons
+        updateNextPrevButtons()
+        
+        // updates labels
+        updateLabels()
+        
+        //Saving flashcards to disk
+        saveAllFlashcardsToDisk()
+        
+    }
+    
+    func updateNextPrevButtons() {
+        // Disables next button if at end
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+            
+        } else{
+            nextButton.isEnabled = true
+        }
+        // Disables prev button if at beginning
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+            
+        } else{
+            prevButton.isEnabled = true
+        }
+        
+    }
+    
+    func updateLabels(){
+        
+        //gets current flashcard
+        let currentFlashcard = flashcards[currentIndex]
+        
+        //updates the labels
+        frontLabel.text = currentFlashcard.question
+        backLabel.text = currentFlashcard.answer
+    }
+    
+    func saveAllFlashcardsToDisk(){
+        
+        //From flashcard array to dictionary array
+        
+        let dictionaryArray = flashcards.map { (card) -> [String: String] in
+            return ["question": card.question, "answer": card.answer]
+        }
+        //Save array on disk using UserDefaults
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        
+        //Log it
+        print("Flashcards saved to UserDefaults")
+    }
+    
+    func readSavedFlashcards() {
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String: String]]{
+            
+            //In here we know for sure we have a dictionary
+            let savedCards = dictionaryArray.map{ dictionary -> Flashcard in
+                return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!)
+            }
+            
+            //Put all these cards in our flashcard array
+            flashcards.append(contentsOf: savedCards)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           // Get the new view controller using segue.destination.
-           let navigationController = segue.destination as! UINavigationController
-           // Pass the selected object to the new view controller.
-           let creationController = navigationController.topViewController as! CreationViewController
-           creationController.flashcardsController = self
+        // Get the new view controller using segue.destination.
+        let navigationController = segue.destination as! UINavigationController
+        // Pass the selected object to the new view controller.
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
         
-       }
+    }
 }
 
--- ReadMe.md
+++ ReadMe.md
@@ -1,21 +1,22 @@
 ## FLASHCARDS
 
-## Lab 2
+## Lab 3
 
 ### App Description
-`TODO://` A flashcard app where you can make a modification of the flashcard.
+`TODO://` Created a flashcard app that allows the user to input their own question and answer to the flashcards. The app also allows you to delete any flashcard that you do not want.
 
 ### App Walk-though
+`TODO://` 
 
-<img src="http://g.recordit.co/I7xayDdMf7.gif width=200><br>
+<img src="https://recordit.co/CEUcOSamRw" width=200><br>
 
+`TODO://` 
 
 ## Required
-- [ X] User can open the creation screen
-- [X ] User can cancel out of the creation screen
-- [X ] User can enter a new question and answer in the creation screen to then show it on the flashcard
+- [X ] User can browse through multiple flashcards
+- [X ] User can re-open the app and see previously created flashcards
 - [X ] Push code to GitHub
 ## Optional
-- [ ] User gets an error if they try to create a new flashcard with no question or answer
+- [ X] User can delete a flashcard
 - [ ] User can edit existing flashcard
-- [ ] User can add multiple choice answers in the creation screen
+- [ ] User can store multiple choice questions
